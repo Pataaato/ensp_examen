060652be9f91deb37d77d66040bca20e47999066
Fretault Samantha <samantha.fretault@gmail.com>
2025-02-18 14:03:51 +0000

    Commit du premier graphique avec les couleurs et le fond

--- app.R
+++ app.R
@@ -22,38 +22,46 @@ ui <- fluidPage(
         choices = c("Oui", "Non"), 
         selected = "Oui", 
         inline = TRUE), 
+      
       selectInput(
         inputId = "filtre_couleurs",
         choices = c("D", "E", "F", "G", "H", "I", "J"),
         label = "Choisir une couleur à filtrer :"), 
+      
       sliderInput(
         inputId = "prix",
         label = "Prix maximum :",
         min = 300,
         max = 20000,
         value = 5000), 
+      
       actionButton(
         inputId = "boutton", 
         label = "Afficher une notification"
       )),
-        mainPanel(
-           plotOutput("distPlot")
+    mainPanel(
+      plotOutput("diamondsPlot"),
+      DTOutput("diamonds_table")
         )
     )
 )
 
 
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
-}
+server <- function(input, output, session) {
+  data_filtered <- reactive({
+    diamonds %>%
+      filter(color == input$filtre_couleurs, price <= input$prix) %>%
+      select(carat, cut, color, clarity, depth, table, price)
+  })
+  output$diamondsPlot <- renderPlot({
+    ggplot(data_filtered(), aes(x = carat, y = price)) +
+      geom_point(color = ifelse(input$couleur_rose == "Oui", "#f3969a", "#5a5a5a")) +
+      labs(
+        title = glue("prix : {input$prix} & color: {input$filtre_couleurs}"),
+        x = "carat",
+        y = "price"
+      ) 
+})}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
